8d5fcabb269afff58b50e181104ad7386a9fff9a
Vinicius Weber <vweber@destaxa.com.br>
2024-06-03 17:07:54 -0300

    feat: update page home, create newProject component

---
+++ src/components/form/Teste.js
@@ -0,0 +1,5 @@
+function Teste() {
+    return <p>Testtnado</p>
+}
+
+export default Teste
---
+++ src/components/project/ProjectForm.js
@@ -0,0 +1,31 @@
+import styles from "./ProjectForm.module.css"
+import Teste from "../form/Teste"
+import LInput from "../form/LInput"
+import Selected from "../form/Selected"
+import SubmitButton from "../form/SubmitButton"
+function ProjectForm() {
+    return (
+        <form className={styles.form}>
+
+            <Teste />
+            <LInput
+                type="text"
+                text="Nome do projeto"
+                name="name"
+                placeholder="Insira o nome do projeto."
+            />
+            <LInput
+                type="number"
+                text="Orçamento do projeto"
+                name="name"
+                placeholder="Insira o orçamento total."
+            />
+
+            <Selected name="category_id" text="Selecione a categoria" />
+            <SubmitButton text="Criar Projeto" />
+
+        </form>
+    )
+}
+
+export default ProjectForm
